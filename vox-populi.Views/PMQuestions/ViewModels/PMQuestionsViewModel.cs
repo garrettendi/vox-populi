c4fc74ee5ed34dee2854024d0ab41bab6d1c5054
Dale Hards <dale.hards@googlemail.com>
2025-07-15 20:22:24 +0100

    Holder commit

---
+++ vox-populi.Views/PMQuestions/ViewModels/PMQuestionsViewModel.cs
@@ -0,0 +1,7 @@
+﻿using vox_populi.Views.ViewModels;
+
+namespace vox_populi.PMQuestions.ViewModels;
+
+public class PMQuestionsViewModel : ViewModelBase
+{
+}
---
+++ vox-populi.Views/PMQuestions/Views/QuestionDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace vox_populi.Avalonia;
+
+public partial class QuestionDialog : Window
+{
+    public QuestionDialog()
+    {
+        InitializeComponent();
+    }
+}
